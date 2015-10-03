b65ff50c78cf46780f73c5093281c9a27309c2ed
Thomas Ruble <ThomasRuble@siu.edu>
2015-10-03 21:39:10 +0000

    achieved a first working oracle script

--- database/db_initializer.py
+++ database/db_initializer.py
@@ -5,3 +5,25 @@ import cx_Oracle
 from config import DBNAME, DBPASSWORD, DBADDRESS
 
 db = cx_Oracle.connect(DBNAME, DBPASSWORD, DBADDRESS)
+c = db.cursor()
+
+txt = ''
+with open('db_script.sql') as script:
+  while True:
+    buf = script.read(1024) 
+    if not buf: break
+    txt += buf
+  
+  commands = txt.split('/')    
+  for command in commands:
+    try:
+      print "Executing: " + command
+      c.execute(command)
+      db.commit()
+    except cx_Oracle.DatabaseError, ex:
+      error, = ex.args
+      print 'Error.code =', error.code
+      print 'Error.message =', error.message
+      print 'Error.offset =', error.offset
+      c.rollback()
+
--- database/db_script.sql
+++ database/db_script.sql
@@ -1,9 +1,9 @@
 create table students
 (sid integer primary key not null, sname varchar(50) not null, 
 major varchar(30) default 'undeclared' not null, 
-s_level varchar(15) not null, age integer);
+s_level varchar(15) not null, age integer)
 /
-create sequence student_id;
+create sequence student_id
 /
 create trigger sid_increment
 before insert on students
