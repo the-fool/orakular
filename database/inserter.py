57bd79d104d110be17bfb2534c2bc1c1598f7f92
Thomas Ruble <sketchbang@gmail.com>
2015-10-04 17:06:59 +0000

    add dummy data

--- database/inserter.py
+++ database/inserter.py
@@ -1,23 +1,28 @@
 import sys, os
 
 header = ['into students (sid, sname, major, s_level, age)',
-  'into department (did, dname)']
+          'into department (did, dname)',
+          'into faculty (fid, fname, deptid)',
+          'into courses (cid, cname, meets_at, room, fid, limit',
+          'into enrolled (sid, cid, exam1, exam2, final)',
+          'into staff (sid, sname, deptid)'
+         ]
            
 with open('dummy.txt') as i:
   with open('data.sql', 'w') as o:
+    index = 0
     o.write('insert all\n')
     while True:
       ln = i.readline()
-      if (ln.rstrip() == 'END'): 
-        print "breaking"
+      if ln.rstrip() == 'BEGIN':
+        while True:
+          ln = i.readline()
+          if ln.rstrip() == 'END': 
+            break
+          o.write('  ' + header[index] + '\n') 
+          o.write('  values ' + ln)
+        index = index + 1
+      else: 
         break
 
-      o.write('into students (sid, sname, major, s_level, age)\n') 
-      o.write('values ' + ln)
-    while True:
-      ln = i.readline()
-      if (ln.rstrip() == 'END\n') or (not ln): 
-        print "broke 2"
-        break
-      o.write('into department\n')
-      o.write('values' + ln)
+    o.write('\nSELECT * FROM dual\n') 
