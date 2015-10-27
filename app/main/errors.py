55dc4dee5104e9f87b7f9b9b9d8ce8d8a6f7e273
Thomas Ruble <sketchbang@gmail.com>
2015-10-27 03:09:06 +0000

    insert database queries to student dashboard

--- app/main/errors.py
+++ app/main/errors.py
@@ -6,5 +6,9 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
-def page_not_found(e):
+def internal_server_error(e):
     return render_template('500.html'), 500
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 500
--- app/student/views.py
+++ app/student/views.py
@@ -1,14 +1,23 @@
-from flask import render_template, url_for, redirect, request, flash
-from flask.ext.login import login_user, logout_user, login_required
+from flask import render_template, url_for, redirect, request, flash, abort
+from flask.ext.login import login_user, logout_user, login_required, current_user
 from . import student
-from ..models import User
+from ..models import User, Student, Enrolled, Course, Faculty
 from .forms import LoginForm
-from ..database import db_session
+from ..database import db_session as sess
 from ..decorators import student_only
 
 @student.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 @student_only
 def profile():
-    return render_template('student/dashboard.html')
+    try:
+        s = sess.query(Student).filter_by(sid=current_user.id).one()
+        e = sess.query(Enrolled).filter_by(sid=s.sid).all()
+        c = {}
+        for x in e:
+            c[x.cid]= sess.query(Course).filter_by(cid=x.cid).one()
+    except:
+        raise
+    
+    return render_template('student/dashboard.html', s=s, e=e, c=c)
 
