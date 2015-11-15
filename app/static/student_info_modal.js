82c2e3ee32efd77cafb2a9f47310fe4c9e9a6b55
Thomas Ruble <sketchbang@gmail.com>
2015-11-15 01:46:30 +0000

    css/jsing --

--- app/static/student_info_modal.js
+++ app/static/student_info_modal.js
@@ -8,8 +8,14 @@ $(document).ready(function() {
 	$('#student_info .modal-body').load('/ajax/student_modal?sid='+id, function() {
 	    $('ul.nav-pills li a').click(function () 
 	     {
+		 var id=$(this).data('id');
+	
 		 $('ul.nav-pills li.active').removeClass('active');
 		 $(this).parent('li').addClass('active');
+
+		 $('.tab-content div.active').toggle()
+		     .removeClass('active').css("opacity", "0");
+		 $('#'+id).addClass('active').toggle().css("opacity", "100");
 	     });
 	});
 	$('#student_info').modal('show');
