cb16343063755561b5ad170016533195bf64d99f
Thomas Ruble <sketchbang@gmail.com>
2015-11-15 15:45:30 +0000

    add average to student info

--- app/static/student_info_modal.js
+++ app/static/student_info_modal.js
@@ -6,16 +6,15 @@ $(document).ready(function() {
 	$('#student_info').data('id', id);
 	$('#student_info .modal-title').text(name);
 	$('#student_info .modal-body').load('/ajax/student_modal?sid='+id, function() {
-	  /*  $('ul.nav-pills li a').click(function () 
-	     {
-		 var id=$(this).attr('href');
-	
-		 $('ul.nav-pills li.active').removeClass('active');
-		 $(this).parent('li').addClass('active');
-		 
-	//	$(this).tab('show');
-	     });*/
-	  
+	  $('.student-info-course-tab', $('#student-info-tabs')).each(function() {
+	      var total = 0;
+	      $('.grade', $(this)).each(function() {
+		  console.log(total);
+		  total += parseFloat($(this).text());
+	      });
+	      $('.course-avg', $(this)).text( (total / 3).toPrecision(3));
+	  });
+	     
 	});
 	$('#student_info').modal('show');
     });
