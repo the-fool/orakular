161f4099d0257df2448fcace2ab9edae35767161
Thomas Ruble <sketchbang@gmail.com>
2015-11-16 19:44:13 +0000

    route view for department splash

--- app/main/views.py
+++ app/main/views.py
@@ -7,6 +7,7 @@ from ..database import db_session as sess
 from ..models import Student, Faculty, Course, Department, Enrolled, Staff, User
 from ..auth.forms import LoginForm
 from ..decorators import non_student_only
+import cx_Oracle
 
 @main.route("/", methods=['GET', 'POST'])
 def index():
@@ -35,10 +36,6 @@ def courses():
     return render_template("courses.html", form=None,
                            courses=course_list) 
 
-@main.route("/departments")
-def departments():
-    return "departments"
-
 
 @main.route("/ajax/student_modal")
 @login_required
@@ -54,14 +51,38 @@ def gen_student_modal():
 
         return render_template('student_modal_gen.html', 
                                s=student, e_list=e_list)
-    except:
+    except cx_Oracle.DatabaseError as e:
+        error, = e.args
+        print "DB Error: {0} -- {1}".format(error.code, error.message)
         raise
+    
+@main.route("/dep")
+def departments():
+    return "filler"
+
+@main.route("/dep/<int:did>")
+def department_home(did):
+    try:
+        dep = sess.query(Department).filter_by(did=did).one()
+        f_list = sess.query(Faculty).filter_by(deptid=did).all()
+        c_list = []
+        for c in sess.query(Course).all():
+            if c.fid in [f.fid for f in f_list]:  # O(n^2)
+                c_list.append(c)
+        return render_template('department_home.html', dep=dep, 
+                               f_list=f_list, c_list=c_list)
+    except cx_Oracle.DatabaseError as e:
+        error, = e.args
+        print "DB Error: {0} -- {1}".format(error.code, error.message)
+        raise
+    
 
 @main.route("/api/<target>")
 def api(target):
     t = globals()[target.title()]
     l = table_to_dict(sess.query(t).all())
     return jsonify({target: l})
+
 
 def table_to_dict(table):
     l = []
