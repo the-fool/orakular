9f86860b5eaf8594125cb385c546f55938caae94
Thomas Ruble <sketchbang@gmail.com>
2015-11-16 16:52:31 +0000

    add validators to edit-grade for prof

--- app/static/dash_faculty.js
+++ app/static/dash_faculty.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     $(".nav-tabs li").first().addClass("active");
     $(".tab-pane").first().addClass("in active");
-    $(".edit-grade").editable();
+    $(".edit-grade").editable({
+	validate: function(value) {
+	    if($.trim(value) == '') {
+		return 'This field is required';
+	    }
+	    if($.isNumeric(value)) {
+		if (value > 100 || value < 0) {
+		    return 'Must be between 0 and 100';
+		}
+	    }
+	    else {
+		return 'Must enter a number';
+	    }
+	}
+    });
 });
