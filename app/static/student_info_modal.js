de0e3b4d6c66f1716992bb6ff204d73896cb9cff
Thomas Ruble <sketchbang@gmail.com>
2015-11-16 18:23:23 +0000

    add student info modal to prof dash

--- app/static/student_info_modal.js
+++ app/static/student_info_modal.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $('tr.student-info').click(function() {
+    $('.student-info-link').click(function() {
 	var id=$(this).data('id');
-	var name=$(this).children().eq(1).text();
-	console.log(name);
+	var name=$('#name-'+id).text();
+	
 	$('#student_info').data('id', id);
 	$('#student_info .modal-title').text(name);
 	$('#student_info .modal-body').load('/ajax/student_modal?sid='+id, function() {
