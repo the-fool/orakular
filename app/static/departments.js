d3b9aa0ec8c7109db896a026f19e0df8a433755f
Thomas Ruble <sketchbang@gmail.com>
2015-11-17 23:46:56 +0000

    set prof names with js, because jinja is too cumbersome

--- app/static/departments.js
+++ app/static/departments.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    $('fname').each(function() {
+ 
+    $('.fname').each(function() {
 	var id = $(this).data('id');
 	var name = $(this).text();
-	$('td.fid-'+id).text(name);
+	 $('.fid-'+id).each(function() {
+	     $(this).text(name)
+	 });
     });
+   
 });
