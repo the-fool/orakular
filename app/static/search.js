48b8499ad07935cf525e5be4bee8b5d5c74c3c54
Thomas Ruble <sketchbang@gmail.com>
2015-11-18 16:01:20 +0000

    vary the search target in search.js

--- app/static/search.js
+++ app/static/search.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $('thead').each(function() {
 	$(this).append("<tr class='warning no-result'><td colspan='5'><i class='fa fa-warning'></i> No result</td></tr>"     );
-	console.log($(this).children());
     });
     var default_table = "student";
     $('#table-'+default_table).bootstrapTable('refresh', {url: "/api/"+default_table});
@@ -10,29 +9,35 @@ $(document).ready(function () {
 	var target = $(e.target).attr("href");
 	target = target.slice(1);
 	$('#table-'+target).bootstrapTable('refresh', {url: "/api/"+target});
+	$('.search').val('');
     });
-    $(".search").keyup(function () {
-	var searchTerm = $(".search").val();
-	var listItem = $('.results tbody').children('tr');
-	var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
-	
-	$.extend($.expr[':'], {'containsi': function(elem, i, match, array){
-            return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-	}});
-	
-	$(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
-	    $(this).attr('visible','false');
+
+    $(".search").each( function() {
+	$(this).keyup(function () {
+	    var table = $(this).data('id');
+	    var searchTerm = $(this).val();
+	    console.log(searchTerm);
+	    var listItem = $('.results-'+id +' tbody').children('tr');
+	    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+	    
+	    $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+		return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+	    }});
+	    
+	    $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+		$(this).attr('visible','false');
+	    });
+	    
+	    $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+		$(this).attr('visible','true');
+	    });
+	    
+	    var jobCount = $('.results tbody tr[visible="true"]').length;
+	    $('.counter').text(jobCount + ' item');
+	    
+	    if(jobCount == '0') {$('.no-result').show();}
+	    else {$('.no-result').hide();}
 	});
-	
-	$(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
-	    $(this).attr('visible','true');
-	});
-	
-	var jobCount = $('.results tbody tr[visible="true"]').length;
-	$('.counter').text(jobCount + ' item');
-	
-	if(jobCount == '0') {$('.no-result').show();}
-	else {$('.no-result').hide();}
     });
     
 });
