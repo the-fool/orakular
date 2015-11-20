0d9d8a7f61fae94186245c3e0cba2e2aed2910af
Thomas Ruble <sketchbang@gmail.com>
2015-11-20 01:09:28 +0000

    add staff dashboard

--- app/staff/views.py
+++ app/staff/views.py
@@ -6,20 +6,13 @@ from .forms import LoginForm
 from ..database import db_session as sess
 from ..decorators import student_only
 
-@student.route('/dashboard', methods=['GET', 'POST'])
+@staff.route('/dashboard', methods=['GET', 'POST'])
 @login_required
-@student_only
+@staff_only
 def dashboard():
     try:
-        s = sess.query(Student).filter_by(sid=current_user.id).one()
-        e = sess.query(Enrolled).filter_by(sid=s.sid).all()
-        c = []
-        for x in e:
-          cobj = sess.query(Course).filter_by(cid=x.cid).one()
-          fobj = sess.query(Faculty).filter_by(fid=cobj.fid).one()
-          c.append({'cobj':cobj, 'fobj':fobj})
+        did = sess.query(Staff.deptid).filter_by(sid=current_user.id).one()
     except:
         raise
-    
-    return render_template('student/dashboard.html', student=s, enrollment=e, courses=c)
+    return render_template('staff/dashboard.html', student=s, enrollment=e, courses=c)
 
